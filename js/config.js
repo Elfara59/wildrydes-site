8a3c45ee7cc7867749398066ae99e150b8ab4b96
Elfara59 <rafaelpiotrowski@yahoo.fr>
2024-11-27 22:49:47 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_CdMz8C3cC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'mbm1d2gniat1qqu80g0jq1gsq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Tu46Xt93x', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5frfbgbp615faph5msg2q6f2o8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
